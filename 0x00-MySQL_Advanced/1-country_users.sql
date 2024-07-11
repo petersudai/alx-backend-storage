8657e1a7053df0d9ac72f980df1d21650c6d40a9
petersudai <psudai@gmail.com>
2024-07-11 20:03:06 +0000

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Create a table named 'users' with specified constraints
+-- id, email, name, country
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+	PRIMARY KEY (id)
+)

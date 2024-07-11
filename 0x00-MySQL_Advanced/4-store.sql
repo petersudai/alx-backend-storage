1fb6f97ad5a6485061d4aa824099ace750b765d9
petersudai <psudai@gmail.com>
2024-07-11 20:57:00 +0000

    Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- Create trigger to decrease quantity
+-- after adding a new order
+CREATE TRIGGER after_insert_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END

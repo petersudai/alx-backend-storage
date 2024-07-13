2aa4fb5f20f9a33a1343b05072661aa9b407bdb8
petersudai <psudai@gmail.com>
2024-07-13 20:52:01 +0000

    Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,20 +3,22 @@
 
 DELIMITER //
 
-CREATE PROCEDURE ComputerAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE weighted_sum FLOAT;
-	DECLARE total_weight INT;
+    DECLARE weighted_sum FLOAT;
+    DECLARE total_weight INT;
 
-	SELECT SUM(c.score * p.weight), SUM(p.weight)
-	INTO weighted_sum, total_weight
-	FROM corrections c
-	JOIN projects p ON c.project_id = p.id
-	WHERE c.user_id = user_id;
+    -- Calculate the weighted sum of the scores
+    SELECT SUM(c.score * p.weight), SUM(p.weight)
+    INTO weighted_sum, total_weight
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
 
-	UPDATE users
-	SET average_score = IF(total_weight = 0, 0, weighted_sum / total_weight)
-	WHERE id = user_id;
+    -- Update the user's average score
+    UPDATE users
+    SET average_score = IF(total_weight = 0, 0, weighted_sum / total_weight)
+    WHERE id = user_id;
 END //
 
 DELIMITER ;

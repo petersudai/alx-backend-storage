55fcb90c045bfc4f30902aac1b803fedd5876d57
petersudai <psudai@gmail.com>
2024-07-13 05:54:50 +0000

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-main.sql
@@ -0,0 +1,17 @@
+Enter password: 
+id  email   name    valid_email
+1   bob@dylan.com   Bob 0
+2   sylvie@dylan.com    Sylvie  1
+3   jeanne@dylan.com    Jeanne  1
+--
+--
+id  email   name    valid_email
+1   bob@dylan.com   Bob 1
+2   sylvie+new@dylan.com    Sylvie  0
+3   jeanne@dylan.com    Jannis  1
+--
+--
+id  email   name    valid_email
+1   bob@dylan.com   Bob 1
+2   sylvie+new@dylan.com    Sylvie  0
+3   jeanne@dylan.com    Jannis  1
---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- SQL script to create a trigger that
+-- resets valid_email when email is changed
+
+DELIMITER //
+
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END //
+
+DELIMITER ;

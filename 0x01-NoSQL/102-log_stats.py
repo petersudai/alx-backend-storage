ae52dafbbd03a091449dd1d59c367442f16d7abf
petersudai <psudai@gmail.com>
2024-07-19 18:49:24 +0000

    Log stats - new version

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -13,17 +13,10 @@ The output includes:
 from pymongo import MongoClient
 
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-PIPE = [
-    {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-    {"$sort": {"count": -1}},
-    {"$limit": 10}
-]
-
 def log_stats(mongo_collection):
     """
     Provides statistics about Nginx logs stored in MongoDB.
-    
+
     Args:
         mongo_collection: pymongo collection object.
     """
@@ -33,7 +26,8 @@ def log_stats(mongo_collection):
 
     # Count of each HTTP method
     print("Methods:")
-    for method in METHODS:
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
         count = mongo_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
@@ -43,10 +37,16 @@ def log_stats(mongo_collection):
 
     # Top 10 most present IPs
     print("IPs:")
-    for ip in mongo_collection.aggregate(PIPE):
+    pipeline = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    for ip in mongo_collection.aggregate(pipeline):
         print(f"\t{ip['_id']}: {ip['count']}")
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     log_stats(nginx_collection)
+
